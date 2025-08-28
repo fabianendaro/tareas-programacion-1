fdf4a44325f3b39c72d5992b7619989e7cf2fb5a
fabianendaro <fabian.endara@ucb.edu.bo>
2025-08-28 11:49:42 -0400

    Add files via upload

---
+++ practica_1/practica_7_ejercicio_7.cpp
@@ -0,0 +1,31 @@
+// Materia: Programación I, Paralelo 4 
+// Autor: Fabian Endara Baptista 
+// Carnet: 10929202 L.P. 
+// Carrera del estudiante: Ingeniería Biomedica 
+// Fecha creación: 25/08/2025 
+// Número de ejercicio: 7 
+#include <iostream>
+#include <cctype> 
+using namespace std;
+main (){
+    char  letra;
+    cout << "Ingrese una letra cualquiera: ";
+    cin >> letra;
+    if(!isalpha(letra))
+    {
+    cout << "Ingreso caracter especial, prueba con una letra";
+    }
+    else if  (letra == 'a' || letra =='e' || letra=='i' || letra=='o' || letra=='u')
+    {
+        cout<<"Ingreso una vocal";
+    }
+    else if  (letra == 'A' || letra =='E' || letra=='I' || letra=='O' || letra=='U')
+    {
+        cout<<"Ingreso una vocal"; 
+    }
+    else
+    {
+        cout<<"Ingreso una consonante";
+    }
+    return 0;
+}
