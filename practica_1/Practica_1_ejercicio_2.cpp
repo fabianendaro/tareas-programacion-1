e765b98a650b2aeb3743717891c588a4825296b6
fabianendaro <fabian.endara@ucb.edu.bo>
2025-08-28 11:26:07 -0400

    Add files via upload

---
+++ practica_1/Practica_1_ejercicio_2.cpp
@@ -0,0 +1,22 @@
+// Materia: Programación I, Paralelo 4 
+// Autor: Fabian Endara Baptista 
+// Carnet: 10929202 L.P. 
+// Carrera del estudiante: Ingeniería Biomedica 
+// Fecha creación: 25/08/2025 
+// Número de ejercicio: 2 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int variable_1 = 0;
+    int variable_2 = 0;
+    cout<< "La variable_1 es: " << variable_1  << endl ;
+    cout<< "La variable_2 es: "<< variable_2 << endl ;
+    cout<<"Cambia el valor de variable_1";
+    cin>> variable_1;
+    cout<<"Cambia el valor de variable_2";
+    cin>> variable_2;
+    cout<< "La variable_1 ahora es: " << variable_1  << endl ;
+    cout<< "La variable_2 ahora es: "<< variable_2 << endl ;
+    return 0;
+}
