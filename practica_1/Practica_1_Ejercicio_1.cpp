60b890df6f130ccd3d4422bbca139e75465f43d3
fabianendaro <fabian.endara@ucb.edu.bo>
2025-08-28 11:29:29 -0400

    Add files via upload

---
+++ practica_1/Practica_1_Ejercicio_1.cpp
@@ -0,0 +1,15 @@
+// Materia: Programación I, Paralelo 4 
+// Autor: Fabian Endara Baptista 
+// Carnet: 10929202 L.P. 
+// Carrera del estudiante: Ingeniería Biomedica 
+// Fecha creación: 25/08/2025 
+// Número de ejercicio: 1 
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hola, soy un estudiante de programacion 1" <<endl;
+    cout << "Mi nombre es Fabian Endara Baptista" << endl;
+    cout << "Estoy en la carrera de Ingenieria Biomedica." << endl;
+    return 0;
+}
